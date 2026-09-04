6dfcb615245e7c7b2b687f6b7ec5396cf8f97972
MYKOLA NESTERENKO <101710505+StingerUA@users.noreply.github.com>
2026-09-04 06:49:43 +0300

    Enable sculpture store enhancements on home entry

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import { StoreApp } from "@/components/store-app";
+import { StoreEnhancements } from "@/components/store-enhancements";
 
 export default function Home() {
-  return <StoreApp initialPath="/tr" />;
+  return (
+    <>
+      <StoreApp initialPath="/tr" />
+      <StoreEnhancements />
+    </>
+  );
 }
